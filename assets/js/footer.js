3dd4a25be38c5532f47a5b96ac311eeaee4cc4c9
Laura <lauravivangoncalves@gmail.com>
2023-09-02 19:44:06 -0300

    Ajuste espaçamento

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,4 +1,4 @@
-let footer = `<footer class="footer rounded p-4 d-flex justify-content-between">
+let footer = `<footer class="footer rounded p-5 d-flex justify-content-between">
 <span class="blog-name"><img src="./assets/img/favicon.png" alt="Planeta Saturno">AstroGuias</span>
 <span>Copyright &copy; 2023</span>
 </footer>`
